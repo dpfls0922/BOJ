0b82956d53912c028ef1a4514f7cae0553fa33fc
dpfls0922 <dpfls0922@naver.com>
2024-07-13 22:22:53 +0900

    study: 큐2

---
+++ Python/큐/18258. 큐2/큐2.py
@@ -0,0 +1,21 @@
+from collections import deque
+import sys
+
+n = int(input())
+
+num = deque()
+for i in range(n):
+    cmd = sys.stdin.readline().strip().split()
+    action = cmd[0]
+    if action == 'push':
+        num.append(cmd[1])
+    elif action == 'pop':
+        print(num.popleft() if num else -1)
+    elif action == 'size':
+        print(len(num))
+    elif action == 'empty':
+        print(0 if num else 1)
+    elif action == 'front':
+        print(num[0] if num else -1)
+    elif action == 'back':
+        print(num[-1] if num else -1)
