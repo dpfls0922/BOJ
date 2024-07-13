05ebeb3a18fd1c6ee0e5ecf3d0906ed21a95d6be
dpfls0922 <dpfls0922@naver.com>
2024-07-13 21:51:01 +0900

    fix: 스택을 이용한 풀이로 시간초과 문제 해결

--- Python/스택/2493. 탑/탑.py
+++ Python/스택/2493. 탑/탑.py
@@ -1,18 +1,30 @@
-// 시간 초과
 n = int(input())
-heights = input().split()
-answer = []
+towers = list(map(int, input().split()))
+answer = [0] * n
+stack = []
 
-while heights:
-    high = 0
-    height = heights.pop()
-    for i in range(len(heights)-1, -1, -1):
-        if height <= heights[i]:
-            high = 1
-            answer.append(i + 1)
+for i in range(n):
+    while stack:
+        if towers[i] >= towers[stack[-1]]:
+            stack.pop()
+        else:
+            answer[i] = stack[-1] + 1
             break
-    if high == 0:
-        answer.append(0)
+    stack.append(i) # 스택에 높이가 큰 타워의 인덱스 담아놓기
+
+print(' '.join(map(str, answer)))
+
+# 시간 초과
+
+# while heights:
+#     high = 0
+#     height = heights.pop()
+#     for i in range(len(heights)-1, -1, -1):
+#         if height <= heights[i]:
+#             high = 1
+#             answer.append(i + 1)
+#             break
+#     if high == 0:
+#         answer.append(0)
     
-answer.reverse()
-print(answer)
+# answer.reverse()
