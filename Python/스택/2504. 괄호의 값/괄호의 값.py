81de6c216b52c65fa1da64a74bd18f0e48e697b3
dpfls0922 <dpfls0922@naver.com>
2024-07-20 23:48:08 +0900

    study: 괄호의 값 풀이 추가

--- Python/스택/2504. 괄호의 값/괄호의 값.py
+++ Python/스택/2504. 괄호의 값/괄호의 값.py
@@ -6,35 +6,26 @@ stack = []
 valid = True
 brackets = input().strip()
 
+# 2차 풀이
 for i in range(len(brackets)):
     if brackets[i] == '(' or brackets[i] == '[':
         stack.append(brackets[i])
-    else: # brackets[i] == ')' or brackets[i] == ']':
+    else: # ')' or ']'
+        sum = 0
+        while stack and isinstance(stack[-1], int):
+            sum += stack.pop()
         if not stack:
             valid = False
             break
         if brackets[i] == ')' and stack[-1] == '(':
             stack.pop()
-            stack.append(2)
+            stack.append(sum * 2 if sum else 2)
         elif brackets[i] == ']' and stack[-1] == '[':
             stack.pop()
-            stack.append(3)
+            stack.append(sum * 3 if sum else 3)
         else:
-            sum = 0
-            while stack and isinstance(stack[-1], int):
-                sum += stack.pop()
-            if not stack:
-                valid = False
-                break
-            if brackets[i] == ')' and stack[-1] == '(':
-                    stack.pop()
-                    stack.append(sum * 2)
-            elif brackets[i] == ']' and stack[-1] == '[':
-                stack.pop()
-                stack.append(sum * 3)
-            else:
-                valid = False
-                break
+            valid = False
+            break
         
 if valid:
     result = 0
@@ -44,9 +35,52 @@ if valid:
         else:
             valid = False
             break
-    if valid:
-        print(result)
-    else:
-        print(0)
+    print(result)
 else:
     print(0)
+
+# 1차 풀이
+# for i in range(len(brackets)):
+#     if brackets[i] == '(' or brackets[i] == '[':
+#         stack.append(brackets[i])
+#     else: # ')' or ']'
+#         if not stack:
+#             valid = False
+#             break
+#         if brackets[i] == ')' and stack[-1] == '(':
+#             stack.pop()
+#             stack.append(2)
+#         elif brackets[i] == ']' and stack[-1] == '[':
+#             stack.pop()
+#             stack.append(3)
+#         else:
+#             sum = 0
+#             while stack and isinstance(stack[-1], int):
+#                 sum += stack.pop()
+#             if not stack:
+#                 valid = False
+#                 break
+#             if brackets[i] == ')' and stack[-1] == '(':
+#                     stack.pop()
+#                     stack.append(sum * 2)
+#             elif brackets[i] == ']' and stack[-1] == '[':
+#                 stack.pop()
+#                 stack.append(sum * 3)
+#             else:
+#                 valid = False
+#                 break
+        
+# if valid:
+#     result = 0
+#     while stack:
+#         if isinstance(stack[-1], int): # 괄호가 아니라 숫자일 때
+#             result += stack.pop()
+#         else:
+#             valid = False
+#             break
+#     if valid:
+#         print(result)
+#     else:
+#         print(0)
+# else:
+#     print(0)
